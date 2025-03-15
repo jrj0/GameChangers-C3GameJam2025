10740843b3f4588bf6935a6c613141636e639bc7
Aarynda <77419463+Aarynda@users.noreply.github.com>
2025-03-14 21:31:37 -0400

    adding ability to change a meter based on a variable

---
+++ GameJamProject/Assets/MeterUpdate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MeterUpdate : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Want this to be roughly generalizable? So need to somehow get a generalized integer to track
+    //For the moment going with a non-generalized version
+
+    private MoneyManager monies;
+    public GameObject money_storage;
+    
+    SpriteRenderer testRenderer;
+
+    public Sprite[] spriteArray;
+
+    void Start()
+    {
+        monies = money_storage.GetComponent<MoneyManager>();
+        testRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int step = 5000 / 13;  //"maximum" money divided by number of sprites in spritesheet
+        int spriteNum = monies.money / step;
+
+        testRenderer.sprite = spriteArray[spriteNum];
+    }
+}
--- GameJamProject/Assets/ReduceMoney.cs
+++ GameJamProject/Assets/ReduceMoney.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ReduceMoney : MonoBehaviour
@@ -5,21 +6,22 @@ public class ReduceMoney : MonoBehaviour
 
     private MoneyManager monies;
     public GameObject money_storage;
-    //public GameObject bad_money_button;
+    public GameObject bad_money_button;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         monies = money_storage.GetComponent<MoneyManager>();
+        bad_money_button = this.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 mousePos = Input.mousePosition;
 
-        if(Input.GetKeyDown(KeyCode.Mouse0)){
-            monies.money -= 25;
-        }
+    }
+
+    void OnMouseDown(){
+        monies.money -= 25;
     }
 }
