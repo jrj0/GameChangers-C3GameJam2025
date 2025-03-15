ff0d5040b7cb53927210677298c876c8e2ec7422
Tyler Nguyen <nguyenhtyler@gmail.com>
2025-03-15 09:53:44 -0400

    Add Text UI to SampleScene and add box collider 2D to button

--- GameJamProject/Assets/ReduceMoney.cs
+++ GameJamProject/Assets/ReduceMoney.cs
@@ -7,12 +7,15 @@ public class ReduceMoney : MonoBehaviour
     private MoneyManager monies;
     public GameObject money_storage;
     public GameObject bad_money_button;
+    
+    public int test;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         monies = money_storage.GetComponent<MoneyManager>();
         bad_money_button = this.gameObject;
+        test = 0;
     }
 
     // Update is called once per frame
@@ -23,5 +26,6 @@ public class ReduceMoney : MonoBehaviour
 
     void OnMouseDown(){
         monies.money -= 25;
+        test += 25;
     }
 }
