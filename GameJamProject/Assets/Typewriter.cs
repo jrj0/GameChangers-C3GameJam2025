60b33ac5058d09713c1cd04d9a9a743e4cd001e0
badwala1 <arjunbadwal007@gmail.com>
2025-03-16 12:42:50 -0400

    TutorialPag Addition

---
+++ GameJamProject/Assets/Typewriter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Typewriter : MonoBehaviour
+{
+    public Text textUI;
+    public float typingSpeed = 0.05f;
+    public string fullText;
+    private int currentCharacterIndex = 0;
+    private string currentText = "";
+    private int textRatio = 0;
+
+    void Start()
+    {
+        if (textUI != null)
+        {
+            textUI.text = "";
+        }
+    }
+
+    void Update()
+    {
+        textRatio++;
+        TypewriterEffect();
+        
+    }
+
+    public void TypewriterEffect()
+    {
+        if (textRatio % 10 == 0)
+        {
+            if (textUI != null && fullText != null && fullText.Length > 0)
+            {
+                if (currentCharacterIndex < fullText.Length)
+                {
+                    currentText = fullText.Substring(0, currentCharacterIndex + 1);
+                    textUI.text = currentText;
+                    currentCharacterIndex++;
+                    Invoke("TypewriterEffect", typingSpeed);
+                }
+                else
+                {
+                    Debug.Log("Typing finished!");
+                }
+            }
+        }
+    }
+}
